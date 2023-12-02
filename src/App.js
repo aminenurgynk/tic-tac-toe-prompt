85c49247147654618cf77c790e16b99ccc3b5881
Aminenur Göynük <amine.gynk@gmail.com>
2023-12-03 01:47:46 +0300

    Game Create Screnn created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import GameCreateScreen from './components/GameCreateScreen/GameCreateScreen';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div>
+      <GameCreateScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListOfGamesScreen/ListOfGamesScreen.js
@@ -0,0 +1,14 @@
+// src/components/ListOfGamesScreen/ListOfGamesScreen.js
+
+import React from 'react';
+
+const ListOfGamesScreen = () => {
+  return (
+    <div>
+      <h1>List of Games</h1>
+      {/* Game listing operations can be added here */}
+    </div>
+  );
+}
+
+export default ListOfGamesScreen;
