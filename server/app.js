9b0e03eb4cabf5421eb0e252a93a28243ab2d3ef
Aminenur Göynük <130973404+aminenurgynk@users.noreply.github.com>
2023-12-08 23:42:10 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ let runID = null;
 const createOpenAI = () => {
   if (!openai) {
     openai = new OpenAI({
-      apiKey: "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",
+      apiKey: process.env.OPENAI_API_KEY,
     });
   }
 };
@@ -23,7 +23,6 @@ const createThread = async () => {
   if (!threadId) {
     const thread = await openai.beta.threads.create();
     threadId = thread.id;
-    //console.log(threadId);
   }
 };
 
@@ -31,9 +30,8 @@ const waitForCompletion = async (thread_id, run_id) => {
   let run = await openai.beta.threads.runs.retrieve(thread_id, run_id);
   while (run.status !== "completed") {
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    //console.log('Waiting for completion...');
     run = await openai.beta.threads.runs.retrieve(thread_id, run_id);
-    //console.log(run);
+ 
   }
   return run;
 };
@@ -42,7 +40,6 @@ app.use(express.json());
 
 app.post("/", async (req, res) => {
   const getData = async (req, res) => {
-    //console.log(req.body.board);
     createOpenAI();
     await createThread();
     console.log(`Requests..........`);
@@ -54,20 +51,15 @@ app.post("/", async (req, res) => {
     });
 
     const run = await openai.beta.threads.runs.create(threadId, {
-      assistant_id: "asst_5aMfjGbdMXC6xPRgGsVce0RJ",
-      instructions:
-        "You are a Tic Tac Toe assistant. You are the most professional Tic Tac Toe player in the world. You have a perfect strategy. Let's play Tic Tac Toe on a 3x3 board. We will play the game in array format ['', '', '', '', '', '', '', '', '']. Write your move in the array I send you, and when you are ready, send it back. Let's start. Provide your answers only as an array (I know you are a text-based artificial intelligence, so let's play this game text-based) and never send me any data expect your answer!",
+      assistant_id: process.env.ASSISTANT_ID,
+     // instructions: Here write your asistant's instructions
     });
 
-    //console.log(run);
     runID = run.id;
     await waitForCompletion(threadId, runID);
 
     const messages = await openai.beta.threads.messages.list(threadId);
     messages.body.data.forEach((message) => {
-      console.log("--------------------------------------------");
-      console.log(message.content[0].text.value);
-      console.log("--------------------------------------------");
     });
     const lastMessage =
       messages.body.data[0].content[0].text.value || "No messages";
